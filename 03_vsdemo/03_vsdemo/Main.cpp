48c4c5297f638c73624c544f9ec7f3c89407924e
陈选磊 <3126418978@qq.com>
2025-08-05 02:46:19 +0800

    20250805

---
+++ 03_vsdemo/03_vsdemo/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	cout<< "demo" << endl;
+	return 0;
+}
--- 03_进制转换/main.cpp
+++ 03_进制转换/main.cpp
@@ -1,8 +1,7 @@
-#include<bits/stdc++.h>
-#define ll long long
+#include <iostream>
 using namespace std;
 string c="012345679ABCDEF";//对应字符。
-void f(int x,int m){
+void f(int x,int m){  //x为十进制整数   m要转换的进制基数
 	if(x/m) f(x/m,m);//继续递归。
 	cout<<c[x%m];//倒序输出。
 }
@@ -12,7 +11,3 @@ int main(){
 	f(x,m);//递归。
 	return 0;
 }
-
-/*
-
-*/
